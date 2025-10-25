1830ded5e67ab4616b8466cd3d1e49540c2a7832
Ethan Kemprowski <ethank27@vt.edu>
2025-10-25 16:33:09 -0400

    Experience section with information and roles/jobs ive done

---
+++ src/content/experience.js
@@ -0,0 +1,33 @@
+export const experience = [
+    {
+        company: "Hexagon",
+        role: "Software Development Intern",
+        location: "Remote",
+        start: "May 2025",
+        end: "Oct 2025",
+        bullets: [
+            "Contributed to Agile sprints, presenting deliverables in cross-team reviews.",
+            "Build a proof-of-concept reporting framework connecting Azure Blob Storage and Application Insights to visualize service health with Python, C#, and Bokeh.",
+            "Automated testing and load-analysis tools (Python + JMeter) to improve QA coverage and measure reliabilit.y",
+            "Designed a centralized DevOps dashboard with HTML/JS widgets powered by KQL, showing live alerts for service performance.",
+            "Implemented and tested new API endpoints supporting multi request workflows within existing C# services.",
+        ],
+        tech: ["Python", "JavaScript", "C#", "KQL", "VSCode", "Visual Studios", "Postman", "Bokeh", "Jupyter Notebook", "Azure DevOps (AppInsights, Wiki/Dashboard)", "Docker Desktop"]
+    },
+    {
+        company: "Ticketmaster",
+        role: "Software Engineer Intern",
+        location: "Reston, VA",
+        start: "Jun 2024",
+        end: "Aug 2024",
+        bullets: [
+            "Worked in a SAFE/Agile environment using Jira for sprint planning and team coordination.",
+            "Developed Tessera, a full stack ticketing platform (React + Flask + SQLite + Stripe with JWT auth and API testing via Postman.",
+            "Extended Java service-metric storage from 2 weeks to 3 years to enhance API analytics and efficiency.",
+            "Built AWS infrastructure with Terraform, Timestream, Lambda, and Eventbridge. Integrated monitoring through CloudWatch and Grafana.",
+            "Streamlined workflows using Gitlab CI, SQL/S3, and IAM",
+        ],
+        tech: ["Python", "JavaScript", "ReactJS", "SQL", "DBBrowser for SQLite", "VSCode", "Postman", "HashiCorp Configuration Language (HCL)", "AWS (Lambda, Timestream, Eventbridge, Cloudwatch, IAM)", "Grafana", "Jira", "Flask", "Vite", "NPM", "Stripe", "Terraform"]
+    }
+    
+]
